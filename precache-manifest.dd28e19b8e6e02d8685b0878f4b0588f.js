e621e16851d99e67a174d0855bea921d2504d72c
Luis Chavarriaga <luischa123@gmail.com>
2020-12-19 19:16:30 -0500

    Initial version
    new repo after retardedly commiting compromising info

---
+++ precache-manifest.dd28e19b8e6e02d8685b0878f4b0588f.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "d997977ebe364f7b199c",
+    "url": "/static/js/main.d997977e.chunk.js"
+  },
+  {
+    "revision": "51178642156e808577d6",
+    "url": "/static/js/1.51178642.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4b87d9d28ef7bed5a0577a2fcadd540d",
+    "url": "/static/media/map-backgrnd.4b87d9d2.jpg"
+  },
+  {
+    "revision": "a8901fe81d178e76bafae5ba7ba7752e",
+    "url": "/static/media/africa_map_backgrnd.a8901fe8.jpg"
+  },
+  {
+    "revision": "0a59653eb6a94e2c944e9d581a3bb6a4",
+    "url": "/static/media/vintage-myspace-backgrnds.0a59653e.jpg"
+  },
+  {
+    "revision": "7fcb19246086d9a9a866078c42cae05d",
+    "url": "/static/media/vidCover.7fcb1924.jpg"
+  },
+  {
+    "revision": "71bf60dc0e80b5fe75b2a95e32bf715b",
+    "url": "/static/media/stockDef.71bf60dc.mp4"
+  },
+  {
+    "revision": "6381aecf24d5dd439e3f9572ac1676df",
+    "url": "/static/media/hammerRight.6381aecf.png"
+  },
+  {
+    "revision": "af35b5db5c6be2ea52dbff2ba7a71421",
+    "url": "/static/media/hammerDown.af35b5db.png"
+  },
+  {
+    "revision": "edaf3fd3420ac53cfd56cf542ce8c113",
+    "url": "/static/media/hammerLeft.edaf3fd3.png"
+  },
+  {
+    "revision": "b1403f5c30bd248a174ce5e88d4b8891",
+    "url": "/static/media/hammerUp.b1403f5c.png"
+  },
+  {
+    "revision": "e8b0088c0dd599633a482368cdab43d2",
+    "url": "/static/media/html5-icon.e8b0088c.png"
+  },
+  {
+    "revision": "19381f275bebc23bc27309d921229c65",
+    "url": "/static/media/css3-icon.19381f27.jpg"
+  },
+  {
+    "revision": "9227288e9703e913c50a312c1260fcba",
+    "url": "/static/media/javascript-icon.9227288e.png"
+  },
+  {
+    "revision": "3986e4dc0eba3b63bd37dff7aac286ac",
+    "url": "/static/media/SQL-icon.3986e4dc.png"
+  },
+  {
+    "revision": "638be90904f9f9248d3c69cb2992c562",
+    "url": "/static/media/react-icon.638be909.svg"
+  },
+  {
+    "revision": "40056865c35728c4d03d333a4efeea3c",
+    "url": "/static/media/stackOverflow-icon.40056865.png"
+  },
+  {
+    "revision": "2d71857446c695f9875e5bfca0de54fe",
+    "url": "/index.html"
+  }
+];
